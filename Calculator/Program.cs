dc3cca85f5218a49b3a8e73a86d9030ef212ee54
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-04-26 13:46:01 +0300

    Update Program.cs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,14 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Calculator<int> calc1 = new Calculator<int>();
+            Calculator<double> calc2 = new Calculator<double>();
+            Calculator<string> calc3 = new Calculator<string>();
+
+            Console.WriteLine($"add = {calc1.Add(5, 4)}\tsub = {calc1.Sub(5,4)}\tmult = {calc1.Mult(5, 4)}\tdiv = {calc1.Sub(5, 0)}");
+            Console.WriteLine($"add = {calc2.Add(5.4, 4)}\tsub = {calc2.Sub(5.4, 4)}\tmult = {calc2.Mult(5.4, 4)}\tdiv = {calc2.Div(5, 0)}");
+            Console.WriteLine($"add = {calc3.Add("5", "4")}");
+            Console.ReadKey();
         }
     }
 }
